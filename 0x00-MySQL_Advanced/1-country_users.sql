cb987cf81529f279c39d528bd666dc9f89e0ed55
Greenfield01 <greenfieldadolphus22@gmail.com>
2023-07-12 22:23:56 +0100

    test

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,9 +1,8 @@
--- creates a table users
+-- Creates a table with unique users.
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	email VARCHAR(255) NOT NULL UNIQUE,
-	name VARCHAR(255)
-	country CHAR(2) NOT NULL DEFULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+	name VARCHAR(255),
+	country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
 );
-
